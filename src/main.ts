de393a2fa731a9518edbc3fd428c823a8d29b8aa
haha <1732976075@qq.com>
2022-03-23 18:21:05 +0800

    创建四个基本页面及导航栏组件

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Navigation from "@/components/Navigation.vue"
 
 Vue.config.productionTip = false
+// eslint-disable-next-line vue/multi-word-component-names
+Vue.component("Navigation", Navigation)
 
 new Vue({
   router,
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
   transpileDependencies: true,
   lintOnSave: false
-})
+}
