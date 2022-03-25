1c40fd29129965128b088be26e809311f8a8e988
haha <1732976075@qq.com>
2022-03-26 02:50:32 +0800

    实现编辑标签

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, { RouteConfig } from 'vue-router'
 import Money from "@/views/Money.vue"
 import Labels from "@/views/Labels.vue"
 import Statistics from "@/views/Statistics.vue"
+import EditLabel from "@/views/EditLabel.vue"
 import NotFound from "@/views/NotFound.vue"
 
 Vue.use(VueRouter)
@@ -24,6 +25,11 @@ const routes: Array<RouteConfig> = [
     path: '/statistics',
     component: Statistics
   },
+  {
+    path: '/labels/edit',
+    component: EditLabel
+  },
+
   {
     path: '*',
     component: NotFound
