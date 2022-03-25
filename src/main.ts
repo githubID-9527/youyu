775c2bb3fc70ec3b16419684209e32feb0560389
haha <1732976075@qq.com>
2022-03-25 16:22:47 +0800

    创建全局 Icon 组件

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import router from './router'
 import store from './store'
 import Navigation from "@/components/Navigation.vue"
 import Layout from "@/components/Layout.vue"
+import Icon from "@/components/Icon.vue"
 
 Vue.config.productionTip = false
 // eslint-disable-next-line vue/multi-word-component-names
 Vue.component("Navigation", Navigation)
 Vue.component("Layout", Layout)
+Vue.component("Icon", Icon)
 
 new Vue({
   router,
