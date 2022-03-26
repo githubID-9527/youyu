e6c1bfd117d3279585756f814b1bf82251e66371
haha <1732976075@qq.com>
2022-03-26 17:35:46 +0800

    封装 tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,23 +1,28 @@
 type RecordItem = {
-    tags: string[];
-    notes: string;
-    type: string;
-    amount: number;
-    createdAt?: Date
-  };
-  type Tag = {
-    id: string;
-    name: string;
-  }
-  type TagListModel = {
-    data: Tag[]
-    fetch: () => Tag[]
-    create: (name: string) => 'success' | 'duplicated' // 联合类型
-    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
-    remove: (id: string) => boolean
-    save: () => void
-  }
-  
-  interface Window {
-    tagList: Tag[]
-  }
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+  createdAt?: Date;
+};
+type Tag = {
+  id: string;
+  name: string;
+};
+type TagListModel = {
+  data: Tag[];
+  fetch: () => Tag[];
+  create: (name: string) => "success" | "duplicated"; // 联合类型
+  update: (id: string, name: string) => "success" | "not found" | "duplicated";
+  remove: (id: string) => boolean;
+  save: () => void;
+};
+
+interface Window {
+  tagList: Tag[];
+  createTag: (name: string) => void;
+  createTag: (name: string) => void;
+  findTag: (id: string) => Tag | undefined;
+  removeTag: (id: string) => boolean;
+  updateTag: (id: string, name: string) => "success" | "not found" | "duplicated";
+}
